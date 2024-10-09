fbd0941a59eaa2cc71f7cb761d0b5e1a4d90fd4a
KhoanDoanVan <doanvankhoan124@gmail.com>
2024-10-09 15:58:37 +0700

    Initial Commit

---
+++ InteractWidget/InteractWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractWidgetApp.swift
+//  InteractWidget
+//
+//  Created by Đoàn Văn Khoan on 9/10/24.
+//
+
+import SwiftUI
+
+@main
+struct InteractWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
